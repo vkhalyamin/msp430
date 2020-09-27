2059fe4c57d110a15825f2d78e6b6dd90fc763fe
Khalyamin Vladimir <vkhalyamin@protonmail.com>
2020-09-27 17:46:46 +0300

    indent fix

--- seven-segment_display/main.c
+++ seven-segment_display/main.c
@@ -13,29 +13,29 @@ void delay(void) {
 
 int main(void)
 {
-	WDTCTL = WDTPW + WDTHOLD;		/* Stop watchdog timer */
+	WDTCTL = WDTPW + WDTHOLD;			/* Stop watchdog timer */
 	
 	SEV_SEG_init();					/* Dispaly initialization */
   
-	for (;;) {						/* Endless loop */
+	for (;;) {					/* Endless loop */
 		
-		for (i; i < 0x10; i++) {    /* i = 0, 1, ... , E, F (0x10_hex = 16_dec) */
-			SEV_SEG_showDigit(i);   /* Show current 'i' value on display */
+		for (i; i < 0x10; i++) { 		/* i = 0, 1, ... , E, F (0x10_hex = 16_dec) */
+			SEV_SEG_showDigit(i);		/* Show current 'i' value on display */
 			delay();
-			SEV_SEG_clear();        /* Clear display (all segments are off) */
+			SEV_SEG_clear();		/* Clear display (all segments are off) */
 			delay();
 		}
 		
-		for (i; i > 0; i -= 2) {      /* i = F, D, C, A, 8, ... , 2, 0 */
-			SEV_SEG_showDigit(ZERO);  /* Show zero on display */
+		for (i; i > 0; i -= 2) {		/* i = F, D, C, A, 8, ... , 2, 0 */
+			SEV_SEG_showDigit(ZERO);	/* Show zero on display */
 			delay();
-			SEV_SEG_showDigit(FOUR);  /* Show four on display */
+			SEV_SEG_showDigit(FOUR);	/* Show four on display */
 			delay();
-			SEV_SEG_showDigit(EIGHT); /* Show eight on display */
+			SEV_SEG_showDigit(EIGHT);	/* Show eight on display */
 			delay();
-			SEV_SEG_showDigit(Fh);    /* Show F on display */
+			SEV_SEG_showDigit(Fh);		/* Show F on display */
 			delay();
-			SEV_SEG_clear();          /* Clear display (all segments are off) */		
+			SEV_SEG_clear();		/* Clear display (all segments are off) */		
 			delay();
 			delay();
 		}
